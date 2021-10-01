4d234c7684ac09e4cf3a51104a3a29f8b38fdc33
ivankiv01 <ivankiv01112001@gmail.com>
2021-10-01 17:27:32 +0300

    создал шапку

--- index.php
+++ index.php
@@ -1,52 +1,38 @@
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang=en>
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Шапка для сайта</title>
 </head>
 <body>
-
-<div id="block-body">
-
-   <!-- //  в тегах header прописана наша шапка-->
-
-    <header>
-
-<!--        // разметка для логотипа-->
-
-        <div class="logo">
-            <a href="index.php">
-                <span class="use">LOVE</span> <span class="web">PHP</span>
+<header class="p-3 bg-dark text-white flex-wrap">
+    <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <a href="/" class="navbar-brand d-flex align-items-center">
+                <p class="use">PHP</p>
+                <p class="web">Love</p>
             </a>
-            <p>NIX solutions</p>
-        </div>
 
-<!--        // разметка для нашего меню-->
-
-        <div class="top-menu">
-            <ul>
-                <li><a class="clickMenu" href="#">Уроки</a></li>
-                <li><a href="#">Урок 1</a></li>
-                <li><a href="#">Урок 2</a></li>
+            <ul class="nav col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0" style="margin-left: 50px">
+                <li><a href="#" class="nav-link px-2 text-white">Домой</a></li>
             </ul>
+
+            <ul class="nav col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="#" class="nav-link px-2 text-white">Уроки</a></li>
+            </ul>
+
+            <div class="text-end">
+                <button type="button" class="btn btn-outline-light me-2">Вход</button>
+                <button type="button" class="btn btn-warning">Зарегестрироваться</button>
+            </div>
         </div>
-
-<!--        // блок с авторизацией-->
-
-        <div class="block-top-auth">
-            <p><a href="#">Вход</a></p>
-            <p><a href="#">Регистрация</a></p>
-        </div>
-
-    </header>
-
-    <div id="block-content"></div>
-
-</div>
-
-
-
+    </div>
+</header>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
 </body>
 </html>
