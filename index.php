f5f44618b2152cb0d38abc644a6eeece8c7ab626
ivankiv01 <ivankiv01112001@gmail.com>
2021-10-01 19:20:22 +0300

    создал шапку и подвал

--- index.php
+++ index.php
@@ -8,29 +8,87 @@
     <title>Шапка для сайта</title>
 </head>
 <body>
-<header class="p-3 bg-dark text-white flex-wrap">
-    <div class="container">
-        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-            <a href="/" class="navbar-brand d-flex align-items-center">
-                <p class="use">PHP</p>
-                <p class="web">Love</p>
-            </a>
+<header class="bg-dark text-white flex-wrap">
+    <nav>
+        <div class="container">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                <div class="col-md-3 py-2 d-md-inline-block">
+                    <a href="/" class="navbar-brand d-flex align-items-center">
+                        <h1 class="use">Love</h1>
+                        <h1 class="web">PHP</h1>
+                    </a>
+                </div>
+                <div class="col-md-6 py-2 d-md-inline-block">
+                    <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                            Выберете урок
+                        </button>
+                        <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
+                            <li><a class="dropdown-item" href="#">Урок 1</a></li>
+                            <li><a class="dropdown-item" href="#">Урок 2</a></li>
+                            <li><a class="dropdown-item" href="#">Урок 3</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Катка в Доту</a></li>
+                        </ul>
+                    </div>
+                </div>
 
-            <ul class="nav col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0" style="margin-left: 50px">
-                <li><a href="#" class="nav-link px-2 text-white">Домой</a></li>
-            </ul>
-
-            <ul class="nav col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                <li><a href="#" class="nav-link px-2 text-white">Уроки</a></li>
-            </ul>
-
-            <div class="text-end">
-                <button type="button" class="btn btn-outline-light me-2">Вход</button>
-                <button type="button" class="btn btn-warning">Зарегестрироваться</button>
+                <div class="text-end py-2 d-md-inline-block">
+                    <button type="button" class="btn btn-outline-light me-2">Вход</button>
+                    <button type="button" class="btn btn-warning">Зарегестрироваться</button>
+                </div>
             </div>
         </div>
-    </div>
+    </nav>
 </header>
+
+<main>
+    <div class="container">
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+
+    </div>
+</main>
+<div class="container">
+    <footer class="py-3 my-4">
+        <ul class="nav justify-content-center pb-3 mb-3">
+            <li class="nav-item"><a href="#" class="nav-link px-2 zazi">На фонтан</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 zazi">На рошана</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 zazi">Mid or fid</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 zazi">Лавка</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 zazi">Нам нужны варды</a></li>
+        </ul>
+        <p class="text-center zaz">© 2021 Company, Inc</p>
+    </footer>
+</div>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
         crossorigin="anonymous"></script>
