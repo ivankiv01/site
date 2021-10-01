8cca61e4196d59898ee9c20000424219588f6636
ivankiv01 <ivankiv01112001@gmail.com>
2021-10-01 14:41:40 +0300

    создал шапку

--- index.php
+++ index.php
@@ -1,14 +1,53 @@
 
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title>Пример веб-страницы</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Шапка для сайта</title>
 </head>
 <body>
-<h1>nix</h1>
-<!-- Комментарий -->
-<p>nix</p>
-<p>nix</p>
+
+<div id="block-body">
+
+    //  в тегах header прописана наша шапка
+
+    <header>
+
+        // разметка для логотипа
+
+        <div class="logo">
+            <a href="index.html">
+                <span class="use">USE</span>-<span class="web">WEB</span>.ru
+            </a>
+            <p>Разработка- это просто</p>
+        </div>
+
+        // разметка для нашего меню
+
+        <div class="top-menu">
+            <ul>
+                <li><a class="clickMenu" href="#">Темы</a></li>
+                <li><a href="#">Видеоуроки</a></li>
+                <li><a href="#">Об авторе</a></li>
+                <li><a href="#">Обратная связь</a></li>
+            </ul>
+        </div>
+
+        // блок с авторизацией
+
+        <div class="block-top-auth">
+            <p><a href="#">Вход</a></p>
+            <p><a href="#">Регистрация</a></p>
+        </div>
+
+    </header>
+
+    <div id="block-content"></div>
+
+</div>
+
+
+
 </body>
 </html>
